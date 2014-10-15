c45bef7da9377390f9a4ef25ec81d8e48d8e60bc
icyflame <kannan.siddharth12@gmail.com>
2014-10-15 08:06:00 +0530

    Completed the coordinator registration form

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :coordinators
   get 'alumni/index'
 
+  root 'alumni#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
