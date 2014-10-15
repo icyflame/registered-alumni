6058ecb0073e8f712f419046f556a9c27c995905
icyflame <kannan.siddharth12@gmail.com>
2014-10-15 19:29:13 +0530

    Permitted all the parameters that can be changed by activeadmin from the browser interface.

--- app/admin/alumni.rb
+++ app/admin/alumni.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Alumni do
-
+  permit_params :name, :hall, :dept, :year
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
